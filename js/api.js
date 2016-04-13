1621ba3c692a9bff522012bf89a4cf0e4bec9634
m-sch <markusschalk@icloud.com>
2016-04-13 21:19:57 +0200

    sorts course name alphabetically now

--- js/api.js
+++ js/api.js
@@ -95,6 +95,10 @@ function getAllCourses(callback) {
                         data.forEach(function(item) {
                             var course = new Course(item.id, item.fullname);
                             courses.push(course);
+                            // sorts course name alphabetically
+                            courses.sort(function(a, b) {
+                                return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0);
+                            });
                         });
                         callback(courses);
                     }).fail(function() {
