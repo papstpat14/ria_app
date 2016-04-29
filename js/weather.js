cc640b3e23b755fad10a0e97790b1c96f84512c3
Patrick <papst.patrick@gmx.at>
2016-04-29 16:52:10 +0200

    removed testfile

--- js/weather.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Patrick on 29.04.2016.
- */
-window.onload=function(){
-    var x =document.getElementById('motivation').innerText;
-    var y = getWeatherMotivation();
-    x.innerText=y;
-}
