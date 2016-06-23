c70ca45887ac0303ef3af82ce02198431f5711eb
m-sch <markusschalk@icloud.com>
2016-06-23 20:02:35 +0200

    minor ui test changes

--- doc/uitest-setup.md
+++ doc/uitest-setup.md
@@ -1,7 +1,7 @@
-# install selenium in project path
-1. install node
+# Selenium Setup
+1. install node and change to project path
 2. npm install selenium-webdriver
 3. install chrome browser
 4. npm install chromedriver
-5. install chromedriver lib from here: http://chromedriver.storage.googleapis.com/index.html?path=2.9/
-6. input your credentials
+5. input your credentials
+6. change URL according to your configuration
--- uitest/test.js
+++ uitest/test.js
@@ -2,33 +2,38 @@
  Creator: Schalk
  */
 
+// change url if needed
+var host = 'http://localhost:63342/ria_app/index.html';
+
+// libraries
 var webdriver = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
 var assert = require('assert');
 var path = require('chromedriver').path;
 
-// change localhost if needed
-var host = 'http://localhost:63342/ria_app/index.html';
-
+// create selenium service
 var service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
 var browser = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
+// tests
 testLogin('hans', 'hans', false);
 // Your credentials
 testLogin('***', '***', true);
 
+// quit the browser
 browser.quit();
 
 /**
- * 
+ * tests a login with the provided credentials
  * @param username
  * @param pw
  * @param login: true if login was successful
  */
 function testLogin(username, pw, login) {
+    // open browser url
     browser.get(host);
     browser.findElement(webdriver.By.id('username')).sendKeys(username);
     browser.findElement(webdriver.By.id('password')).sendKeys(pw);
